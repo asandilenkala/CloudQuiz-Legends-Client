b33038d1bdbe45bd1cfae6c4f740a3422d70dc62
ZintlanuBozana <bozanazintlanu@gmail.com>
2025-07-15 11:39:04 +0200

    Update App.js
    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './pages/Navbar';
 import Sidebar from './pages/Sidebar';
@@ -13,33 +13,40 @@ import WelcomePage from './pages/WelcomePage';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import SignInForm from './pages/SignInForm';
+import Profile from './pages/Profile';
+import Classroom from './pages/Classroom';
+import SubscriptionForm from './pages/SubscriptionForm';
+
+// Inside <Routes> in App component:
+<Route path="/subscribe" element={<SubscriptionForm />} />
+
 
 function App() {
   return (
-    <BrowserRouter>
-    <Navbar/>
-    <Sidebar/>
-    
-    
-    <div className="main-content">
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/score" element={<Score />} />
-        <Route path="/welcome" element={<WelcomePage/>} />
-        <Route path="sign-in" element={<SignIn/>}/>
-        <Route path="sign-up" element={<SignUp/>}/>
-        <Route path="continue-with-email" element={<SignInForm/>}/>
-      </Routes>
-    </div>  
-    
-    
-    <Footer/>
+    <>
+      <Navbar />
+      <Sidebar />
 
-    </BrowserRouter>
+      <div className="main-content">
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/score" element={<Score />} />
+          <Route path="/welcome" element={<WelcomePage />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/continue-with-email" element={<SignInForm />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/classroom" element={<Classroom />} />
+          <Route path="/subscription" element={<SubscriptionForm />} />
+        </Routes>
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
-export default App; 
+export default App;
